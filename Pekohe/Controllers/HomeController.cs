6c2b98dac7a63a3f0b4bda503094045ce5b45922
Manish Katoch <manish.katoch9@gmail.com>
2021-07-15 23:29:38 +0530

    Latest code

---
+++ Pekohe/Controllers/HomeController.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Pekohe.Models;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Pekohe.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private readonly IWebHostEnvironment _hostEnvironment;
+        public HomeController(ILogger<HomeController> logger, IWebHostEnvironment environment)
+        {
+            _logger = logger;
+            _hostEnvironment = environment;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        [HttpPost]
+        public IActionResult Subscribe(string email)
+        {
+            string myfile = Path.Combine(_hostEnvironment.WebRootPath,"Email.txt");
+            using (FileStream fs = new FileStream(myfile, FileMode.Append, FileAccess.Write))
+            using (StreamWriter sw = new StreamWriter(fs))
+            {
+                sw.WriteLine(email);
+            }
+            return Json(new { success = true, message = "Email saved successfully!" }); 
+        }
+
+    }
+}
